2bab007144b9951860e83926e5c9491aa30e8501
Adilet Segizekov <adilets@gmail.com>
2020-06-04 20:40:00 -0700

    added addition and subtraction methods to vector.py

--- vector.py
+++ vector.py
@@ -10,14 +10,30 @@ class Vec2:
     def norm(self) -> float:
         return sqrt((self.x ** 2) + (self.y ** 2))
 
-    def norm2(self) -> float:
+    def norm_squared(self) -> float:
         return (self.x ** 2) + (self.y ** 2)
 
-    def dot(self, other: "Vec2") -> float:
+    def dot(self, other) -> float:
         return (self.x * other.x) + (self.y * other.y)
 
-    def cross(self, other: "Vec2") -> float:
+    def cross(self, other) -> float:
         return (self.x * other.y) - (self.y * other.x)
+
+    def __add__(self, other) -> "Vec2":
+        return Vec2(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other) -> "Vec2":
+        return Vec2(self.x - other.x, self.y - other.y)
+
+    def __iadd__(self, other):
+        self.x += other.x
+        self.y += other.y
+        return self
+
+    def __isub__(self, other):
+        self.x -= other.x
+        self.y -= other.y
+        return self
 
     def __str__(self):
         return f"({self.x}, {self.y})"
@@ -38,17 +54,35 @@ class Vec3:
     def norm(self) -> float:
         return sqrt((self.x ** 2) + (self.y ** 2) + (self.z ** 2))
 
-    def norm2(self) -> float:
+    def norm_squared(self) -> float:
         return (self.x ** 2) + (self.y ** 2) + (self.z ** 2)
 
-    def dot(self, other: "Vec3") -> float:
+    def dot(self, other) -> float:
         return (self.x * other.x) + (self.y * other.y) + (self.z * other.z)
 
-    def cross(self, other: "Vec3") -> "Vec3":
+    def cross(self, other) -> "Vec3":
         new_x = (self.y * other.z) - (self.z * other.y)
         new_y = (self.z * other.x) - (self.x * other.z)
         new_z = (self.x * other.y) - (self.y * other.x)
         return Vector3(new_x, new_y, new_z)
+
+    def __add__(self, other) -> "Vec3":
+        return Vec3(self.x + other.x, self.y + other.y, self.z + other.z)
+
+    def __sub__(self, other) -> "Vec3":
+        return Vec3(self.x - other.x, self.y - other.y, self.z - other.z)
+
+    def __iadd__(self, other):
+        self.x += other.x
+        self.y += other.y
+        self.z += other.z
+        return self
+
+    def __isub__(self, other):
+        self.x -= other.x
+        self.y -= other.y
+        self.z -= other.z
+        return self
 
     def __str__(self):
         return f"({self.x}, {self.y}, {self.z})"
